8543031d74d31aec5d9f6bf35f1cda8c7b7c3531
koltbern15 <k.t.bern1@gmail.com>
2026-05-25 23:32:13 -0400

    fix(eslint): silence no-html-link-for-pages (App Router only, no /pages dir)

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -13,7 +13,8 @@ export default [
     files: ['**/*.{ts,tsx,js,jsx}'],
     rules: {
       '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
-      'react/no-unescaped-entities': 'off'
+      'react/no-unescaped-entities': 'off',
+      '@next/next/no-html-link-for-pages': 'off'
     }
   }
 ]
