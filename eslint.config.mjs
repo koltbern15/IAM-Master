0060aa9c2ebdb0a182546103da9fff016869e44e
koltbern15 <k.t.bern1@gmail.com>
2026-05-25 23:31:04 -0400

    fix(eslint): restore Next.js flat-config with core-web-vitals rules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,5 +1,6 @@
 import js from '@eslint/js'
 import tseslint from 'typescript-eslint'
+import nextPlugin from '@next/eslint-plugin-next'
 
 export default [
   {
@@ -7,17 +8,9 @@ export default [
   },
   js.configs.recommended,
   ...tseslint.configs.recommended,
+  nextPlugin.configs['core-web-vitals'],
   {
-    files: ['**/*.{ts,tsx}'],
-    languageOptions: {
-      parser: tseslint.parser,
-      parserOptions: {
-        project: true,
-        ecmaFeatures: {
-          jsx: true
-        }
-      }
-    },
+    files: ['**/*.{ts,tsx,js,jsx}'],
     rules: {
       '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
       'react/no-unescaped-entities': 'off'
